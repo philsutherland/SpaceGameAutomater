d4db3565b0da0b5306e0ff27294375eec54538a9
Phil Sutherland <psuth026@uottawa.ca>
2020-01-20 12:32:38 -0500

    Added in multi threading

--- main.py
+++ main.py
@@ -1,9 +1,61 @@
 from selenium import webdriver
 # from target import Target
 import selenium
-import time
+from time import sleep
+from threading import Thread
+
+
+# This class holds the static variables
+class Controller:
+    zeus_fleet_active = False
+    heph_active = False
+
+
+# Changes the status of the zeus fleet once it returns from an attack
+def change_zeus_fleet_status(thread_name, delay):
+    print("Thread: " + thread_name)
+    print(delay)
+    sleep(delay)
+    Controller.zeus_fleet_status = False
+    print("Wake up!")
 
 
 # If this is the first file called, run
 if __name__ == "__main__":
     print("Go Selenium!")
+
+    while True:
+        if Controller.zeus_fleet_active == False:
+            # Find target
+            print("Find target")
+            # Launch at target
+            print("Launch at target")
+
+            Controller.zeus_fleet_active = True
+
+            try:
+                # Create new thread to change zeus fleet status back to False once fleet returns
+                zeus_thread = Thread(target=change_zeus_fleet_status,
+                                     args=("Change Zeus Fleet Status", 10,))
+                # Start the zeus fleet thread
+                zeus_thread.start()
+                print("Starting zeus thread")
+
+                # End zeus thread by joining it with the main thread
+                zeus_thread.join()
+                print("Ending zeus thread")
+
+            except:
+                print("Error: Unable to start zeus thread")
+
+        if Controller.heph_active == False:
+            # Find debris field
+            print("Finding debris field")
+
+            # Move heph to debris field
+            print("Moving heph to debris field")
+
+            # Harvest debris field
+            print("Harvesting debris field")
+
+            # Controller.heph_active = True
