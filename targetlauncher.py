458e52b12b892a805c018f6796a9ccf5256862a7
Phil Sutherland <psuth026@uottawa.ca>
2020-02-02 22:42:00 +0000

    Added more try blocks

--- targetlauncher.py
+++ targetlauncher.py
@@ -1,35 +1,48 @@
 import selenium
 import time
 from target import Target
+from restart import restart
 
 
 # Launch on target
 def target_launcher(browser, target):
     time.sleep(2)
 
-    # Go to solar system
-    browser.get(
-        f"https://uni2.playstarfleetextreme.com/galaxy/show?current_planet=1000000216225&galaxy=1&solar_system={target.system}")
+    try:
+        # Go to solar system
+        browser.get(
+            f"https://uni2.playstarfleetextreme.com/galaxy/show?current_planet=1000000216225&galaxy=1&solar_system={target.system}")
 
-    time.sleep(2)
+        time.sleep(2)
+    except BaseException as e:
+        print(
+            f"Error: Something went wrong while trying to go to galaxy page to launch on target")
+        print(f"Specific Error {e}")
+        restart()
 
-    # Click the attack button
-    browser.find_element_by_xpath(
-        f"//*[@id='select_fleet_link_attack_{target.planet}e']").click()
+    try:
+        # Click the attack button
+        browser.find_element_by_xpath(
+            f"//*[@id='select_fleet_link_attack_{target.planet}e']").click()
 
-    # Select all zeus ships and launch
-    browser.find_element_by_xpath(
-        "//*[@id='assign_fleet_form']/div[1]/div[2]/div/div[4]/div[2]/div/div[1]/input").click()
-    time.sleep(2)
-    travel_time = str(browser.find_element_by_xpath(
-        "//*[@id='task_duration']").text)
-    travel_time = travel_time.split(":")
-    duration = (int(travel_time[0])*3600 +
-                int(travel_time[1])*60 + int(travel_time[2])) * 2
+        # Select all zeus ships and launch
+        browser.find_element_by_xpath(
+            "//*[@id='assign_fleet_form']/div[1]/div[2]/div/div[4]/div[2]/div/div[1]/input").click()
+        time.sleep(2)
+        travel_time = str(browser.find_element_by_xpath(
+            "//*[@id='task_duration']").text)
+        travel_time = travel_time.split(":")
+        duration = (int(travel_time[0])*3600 +
+                    int(travel_time[1])*60 + int(travel_time[2])) * 2
 
-    print(f"Duration is: {duration}s")
+        print(f"Duration is: {duration}s")
 
-    browser.find_element_by_xpath("//*[@id='assign_button']").click()
-    print(f"Zeus fleet launched at: {target.location()}")
+        browser.find_element_by_xpath("//*[@id='assign_button']").click()
+        print(f"Zeus fleet launched at: {target.location()}")
+    except BaseException as e:
+        print(
+            f"Error: Something went wrong while trying to launch on {target.location()}")
+        print(f"Specific Error {e}")
+        restart()
 
     return duration
